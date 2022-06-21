eebd9e85904e2607f146e57491cbbdbe979022b8
Gulshan kumar <gulshansharma498251511@gmail.com>
2022-06-21 16:24:00 +0530

    Update BlogDetail.js

--- college_buddy_frontend/src/components/BlogDetail.js
+++ college_buddy_frontend/src/components/BlogDetail.js
@@ -1,20 +1,34 @@
-import {React} from 'react'
-import { Link } from 'react-router-dom'
+import {React, useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
 
 function BlogDetail() {
   const { id } = useParams();
+  const [detail, setDetail] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch(
+        `http://localhost:3000/api/blog/${id}`
+      );
+      response = await response.json();
+      setDetail(response[0]);
+    }
+    fetchData();
+  }, []);
 
-  const handleClick = () => {
-        async function deletePost() {
-            await fetch(`http://localhost:3000/api/blog/${id}`, { method: 'DELETE' });
-        }
+  // const handleClick = () => {
+  //       async function deletePost() {
+  //           await fetch(`http://localhost:3000/api/blog/${id}`, { method: 'DELETE' });
+  //       }
     
-        deletePost();
-  }
+  //       deletePost();
+  // }
   return (
     <div>
-        <button btn-danger onClick={handleClick}>Delete!</button>
+       {detail &&(
+        <div>
+          <h1>{detail.blog_data}</h1>
+        </div>
+       )}
     </div>
   )
 }
